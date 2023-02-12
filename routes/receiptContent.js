9baea4549b5b44d7148d3abb55b66de1066a9c0f
waizshahid <waizshahid.ws@gmail.com>
2023-02-12 12:33:22 +0500

    removed receiptContent

---
+++ routes/receiptContent.js
@@ -0,0 +1,30 @@
+
+var express = require("express");
+var router = express.Router();
+var cookie = require('js-cookie')
+const request = require('request')
+
+
+router.get('/receipt/:id', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
+
+router.post('/receipt/:id', (req, res) => {
+    const options = {
+      url: 'https://rehubcy.com/api/receipt/' + req.params.id,
+      method: 'POST',
+      body: req.body,
+      json: true
+    }
+  
+    request(options, (error, response, body) => {
+      if (error) {
+        console.error(error)
+        res.status(500).send('Internal server error')
+      } else {
+        res.send('Payment complete')
+      }
+    })
+  })
+
+module.exports = router;
