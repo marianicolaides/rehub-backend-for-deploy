867134ba9051b57857bb45913a0ee4d27303e525
waizshahid <waizshahid.ws@gmail.com>
2023-02-12 11:47:00 +0500

    added post method for /receipt/:id

--- routes/receiptContent.js
+++ routes/receiptContent.js
@@ -2,29 +2,24 @@
 var express = require("express");
 var router = express.Router();
 var cookie = require('js-cookie')
+const request = require('request')
 
-// router.post("/receipt/:id", async (req, res) => {
-//     // const data = req.body;
-//     // cookie.set("ResponseData",data)
-
-//     // // res.status(200)
-//     // const data = req.body;
-//     // console.log("Received data:", data);
-//     try {
-//         return res.status(400).json("User does not exist");     
-//     } catch (err) {
-//         console.error(err)
-//     }
-   
-
-// })
-
-router.post('/receipt/:id', (req, res) => {
-    // const data = req.body;
-    // cookie.set("ResponseData",data)
-
-	res.send(req.body);
-
-});
+app.post('/receipt/:id', (req, res) => {
+    const options = {
+      url: 'https://rehubcy.com/api/receipt/' + req.params.id,
+      method: 'POST',
+      body: req.body,
+      json: true
+    }
+  
+    request(options, (error, response, body) => {
+      if (error) {
+        console.error(error)
+        res.status(500).send('Internal server error')
+      } else {
+        res.send('Payment complete')
+      }
+    })
+  })
 
 module.exports = router;
