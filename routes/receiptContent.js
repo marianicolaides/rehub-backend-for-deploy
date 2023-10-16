1bebf947fe64fed50421de3e4166a1a912fac428
vlad.u <v.uvarov@exeliatech.com>
2023-10-16 12:24:53 +0200

    fix jcc redirection error

--- routes/receiptContent.js
+++ routes/receiptContent.js
@@ -1,30 +1,5 @@
-
 var express = require("express");
 var router = express.Router();
-var cookie = require('js-cookie')
-var path = require('path')
-const request = require('request')
 
-
-
-
-router.all('/receipt/:id', (req, res) => {
-    const options = {
-      url: 'https://rehubcy.com/api/receipt/' + req.params.id,
-      method: 'POST',
-      body: req.body,
-      json: true
-    }
-  
-    request(options, (error, response, body) => {
-      if (error) {
-        console.error(error)
-        res.status(500).send('Internal server error')
-      } else {
-      
-        res.redirect(`https://www.rehubcy.com/receipt/${req.params.id}`)
-      }
-    })
-  })
 
 module.exports = router;
